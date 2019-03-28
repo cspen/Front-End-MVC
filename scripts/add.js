cf2323ba15de28ce05a1932c11fb193dcfd85a3d
unknown <cspen@modintro.com>
2019-03-27 23:32:13 -0500

    Set logic flow

--- scripts/add.js
+++ scripts/add.js
@@ -1,13 +1,13 @@
 function functionName(etn) {
 
 	console.log(" * * * * " + etn.type);
-	if(etn.nodeName === "A") {
-		if(etn.id === "click-link") {
-			alert("LINK CLICKED");
+	if(etn.target.nodeName === "A") {
+		if(etn.target.id == "click-link") {
+			console.log("LINK CLICKED");
 		}
-	} else if(etn.nodeName === "INPUT") {
-		if(etn.id === "output") {
-			alert("OUTPUT");
+	} else if(etn.target.nodeName === "INPUT") {
+		if(etn.target.id == "output") {
+			console.log("OUTPUT");
 		}
 	}
 }
